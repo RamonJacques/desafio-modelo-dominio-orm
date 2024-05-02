b93ee2a5c7a83b490e30848fab9a05a2ba9af739
RamonJacques <ramonjacquesrs@gmail.com>
2024-05-02 10:36:52 -0300

    Project created

---
+++ src/main/java/com/ramon/desafiomdorm/DesafioMdOrmApplication.java
@@ -0,0 +1,13 @@
+package com.ramon.desafiomdorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMdOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMdOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramon/desafiomdorm/DesafioMdOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramon.desafiomdorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMdOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
