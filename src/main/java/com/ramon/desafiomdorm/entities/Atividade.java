0fddcc635bf6fd9ad75ebc181bff9d3611c3f60e
RamonJacques <ramonjacquesrs@gmail.com>
2024-05-02 12:56:54 -0300

    entities Bloco, Participante

--- src/main/java/com/ramon/desafiomdorm/entities/Atividade.java
+++ src/main/java/com/ramon/desafiomdorm/entities/Atividade.java
@@ -1,11 +1,18 @@
 package com.ramon.desafiomdorm.entities;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,7 +22,7 @@ public class Atividade {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String name;
+	private String nome;
 	private String descricao;
 	private Double preco;
 	
@@ -23,12 +30,21 @@ public class Atividade {
 	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 	
+	@ManyToMany
+	@JoinTable(name = "tb_atividade_participante",
+			joinColumns = @JoinColumn(name = "atividade_id"),
+			inverseJoinColumns = @JoinColumn(name = "participante_id"))
+	private Set<Participante> participantes = new HashSet<>();
+	
+	@OneToMany(mappedBy = "atividades")
+	private Set<Bloco> blocos = new HashSet<>();
+	
 	public Atividade() {
 	}
 
-	public Atividade(Integer id, String name, String descricao, Double preco, Categoria categoria) {
+	public Atividade(Integer id, String nome, String descricao, Double preco, Categoria categoria) {
 		this.id = id;
-		this.name = name;
+		this.nome = nome;
 		this.descricao = descricao;
 		this.preco = preco;
 		this.categoria = categoria;
@@ -42,12 +58,12 @@ public class Atividade {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public String getDescricao() {
@@ -72,5 +88,30 @@ public class Atividade {
 
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
+	}
+
+	public Set<Participante> getParticipantes() {
+		return participantes;
+	}
+
+	public Set<Bloco> getBlocos() {
+		return blocos;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Atividade other = (Atividade) obj;
+		return Objects.equals(id, other.id);
 	}
 }
--- src/main/java/com/ramon/desafiomdorm/entities/Participante.java
+++ src/main/java/com/ramon/desafiomdorm/entities/Participante.java
@@ -1,34 +1,39 @@
 package com.ramon.desafiomdorm.entities;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_categoria")
-public class Categoria {
+@Table(name = "tb_participante")
+public class Participante {
 
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String descricao;
+	private String nome;
 	
-	@OneToMany(mappedBy = "categoria")
-	List<Atividade> atividades =new ArrayList<>();
+	@Column(unique = true)
+	private String email;
 	
-	public Categoria() {
+	@ManyToMany(mappedBy = "participantes")
+	private Set<Atividade> atividades = new HashSet<>();
+	
+	public Participante() {
 	}
 
-	public Categoria(Integer id, String descricao) {
+	public Participante(Integer id, String nome, String email) {
 		this.id = id;
-		this.descricao = descricao;
+		this.nome = nome;
+		this.email = email;
 	}
 
 	public Integer getId() {
@@ -39,15 +44,23 @@ public class Categoria {
 		this.id = id;
 	}
 
-	public String getDescricao() {
-		return descricao;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
-	public List<Atividade> getAtividades() {
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<Atividade> getAtividades() {
 		return atividades;
 	}
 
@@ -64,7 +77,7 @@ public class Categoria {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Participante other = (Participante) obj;
 		return Objects.equals(id, other.id);
 	}
 }
